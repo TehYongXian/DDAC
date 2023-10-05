04619ca5cf072552f280c45c0c1cf2ef80605cfc
haoyu.choong@repromarketing.com <haoyu.choong@repromarketing.com>
2023-10-06 06:56:01 +0800

    sns for payment after bicycle reservation

---
+++ mvcflowershoplab1/mvcflowershoplab1/Models/PaymentInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace mvcflowershoplab1.Models
+{
+    public class PaymentInfo
+    {
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CVV { get; set; }
+        public string Email { get; set; }
+        public string ReserveID { get; set; }
+    }
+}
